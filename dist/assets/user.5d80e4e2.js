c3243b2a97dcf99f50239f122d62779725bf08fd
jason06286 <dj4871114@gmail.com>
2022-06-29 15:41:04 +0800

    feat: google callback

--- dist/assets/user.5d80e4e2.js
+++
@@ -1,1 +0,0 @@
-import{d as n,r as u}from"./index.bb95e3cf.js";const i=n("user",()=>{const e=u({});return{user:e,setUser:s=>{const{name:r,photo:t,_id:o,...a}=s;e.value={name:r,photo:t,id:o,data:a}}}});export{i as u};
