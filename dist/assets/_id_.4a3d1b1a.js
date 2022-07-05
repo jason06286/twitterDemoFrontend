a5c7211f6c1a1c666c4dff5f71afbfd42517aa17
jason06286 <dj4871114@gmail.com>
2022-07-05 16:49:28 +0800

    add posts page

---
+++ dist/assets/_id_.4a3d1b1a.js
@@ -0,0 +1,1 @@
+import{q as e,o as t,c as s,t as a,u as r}from"./index.aa483a1e.js";const p={__name:"[id]",setup(c){const o=e();return console.log("route :>> ",o),console.log("route.params :>> ",o.params.id),(n,l)=>(t(),s("div",null,a(`Welcome to post  ${r(o).params.id}`),1))}};export{p as default};
