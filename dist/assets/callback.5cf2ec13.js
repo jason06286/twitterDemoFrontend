fd68c5b7d9843517e43e12ed4bb218be3bc5fe7d
jason06286 <dj4871114@gmail.com>
2022-08-29 20:19:07 +0800

    add: dist

---
+++ dist/assets/callback.5cf2ec13.js
@@ -0,0 +1,1 @@
+import{u as n,o as u,a as d,f as p,j as k,b as _,aj as i,s as m}from"./index.f69a5cd7.js";const x={__name:"callback",setup(l){const t=k(),s=_(),o=n(),{token:a,id:r}=t.query,c=()=>{const e=i().utc().add(7,"days").format();document.cookie=`twitterToken=${a}; expires=${e}; path=/`,m(),o.setUser(r),s.push("/")};return u(()=>{c()}),(e,f)=>(d(),p("div"))}};export{x as default};
